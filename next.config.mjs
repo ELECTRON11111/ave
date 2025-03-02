48040cf19f9911fa640cb8306b296213e595aa64
daniel_eln <omoniyiopemipo6@gmail.com>
2025-03-02 12:51:30 +0100

    Added next.config.mjs file to try to get render deployment working

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // In order to create a static site
+    output: "export",
+    reactStrictMode: true,
+  };
+  
+  export default nextConfig;
